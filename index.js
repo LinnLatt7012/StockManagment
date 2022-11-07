026daa60b0be5b176350c7ee5ad430177056db0c
LinnLattOo <linnlattoo@gusto.edu.mm>
2022-11-07 17:45:11 +0630

    database sync update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4000;
 app.listen(port, async () => {
     try {
         await sequelize.authenticate();
-        await sequelize.sync({ alter: true });
+        await sequelize.sync({ force: true });
         console.log("Connection has been established successfully.");
     } catch (error) {
         console.error("Unable to connect to the database:", error);
