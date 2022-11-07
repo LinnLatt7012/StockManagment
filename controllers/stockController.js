b4bb3b65d4d37f50bed5946741365c47d5931e31
LinnLattOo <linnlattoo@gusto.edu.mm>
2022-11-08 05:14:05 +0630

    update tableName

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -27,7 +27,8 @@ exports.allStocks = async (req, res) => {
     try {
         var { nofDays } = req.query;
         nofDays = nofDays || 7;
-        const sql = `SELECT P.mmName,P.enName,S.status,V.unitPrice,S.quantity, S.quantity*V.unitPrice as total FROM stock_details as S LEFT JOIN product_vers as V ON S.version=V.id Left JOIN raw_products as P ON S.productID=P.productID WHERE 
+        const sql = `SELECT P.mmName,P.enName,S.status,V.unitPrice,S.quantity, S.quantity*V.unitPrice as total 
+        FROM Stock_Details as S LEFT JOIN Product_Vers as V ON S.version=V.id Left JOIN Raw_Products as P ON S.productID=P.productID WHERE 
         DATE(S.date) > SUBDATE(CURDATE(), ${nofDays}) Order by S.date desc`;
         const result = await sequelize.query(sql, { type: QueryTypes.SELECT });
         return res.send({
@@ -53,9 +54,9 @@ exports.totalStock = async (req, res) => {
             : "sd.status=1";
     // console.log(statusCon);
     const sql = `SELECT SUM(TS.TOTAL) as TotalStockValue FROM (SELECT S.date,SUM(S.quantity)*V.unitPrice as TOTAL 
-    FROM (SELECT * FROM stock_details as sd WHERE 
+    FROM (SELECT * FROM Stock_Details as sd WHERE 
     (DATE(sd.date) > SUBDATE(CURDATE(), ${nofDays})) AND (${statusCon})) AS S 
-    LEFT JOIN product_vers as V ON S.version=V.id GROUP BY S.productID,S.version ) as TS`;
+    LEFT JOIN Product_Vers as V ON S.version=V.id GROUP BY S.productID,S.version ) as TS`;
     const result = await sequelize.query(sql, { type: QueryTypes.SELECT });
     return res.send({
         meassage: "total",
